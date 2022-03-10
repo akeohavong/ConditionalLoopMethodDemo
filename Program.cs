15fdb5c9d661edaa2cbe983dad1227eca20fc2e3
Anou Keohavong <akeohavong5@gmail.com>
2022-03-10 09:54:53 -0600

    added within range method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,30 @@ namespace ConditionalLoopMethodDemo
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string fname = GetRequiredString("Enter you first name: ");
+            Console.WriteLine($"First name: {fname}");
+
+            Console.WriteLine($"The numbers 10, 23, 3 is {IsWithinRange(23, 10, 3)}");
+        }
+
+        private static string GetRequiredString(string prompt)
+        {
+            string input = "";
+
+            while (string.IsNullOrEmpty(input)) {
+                Console.Write(prompt);
+                input = Console.ReadLine();
+
+            }
+            return input;
+        }
+
+        //access modifier - static(no not) - return type - name (params in here)
+        private static bool IsWithinRange(int num, int target, int range)
+        {
+            return (num % target < range);
         }
     }
 }
